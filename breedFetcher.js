26896804da81317c125e6c71c90fb1459c7cf41c
analuciachin <ananime@gmail.com>
2020-12-03 21:17:46 +0000

    worked on refactoring the code

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,24 +1,11 @@
 const request = require('request');
-const breed = process.argv.slice(2);
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed[0]}`, function(error, response, body) {
-  
-  // console.error('error:', error);
-  console.log('statusCode:', response && response.statusCode);
-
-  if (error) {
-    console.log(error);
-  } else {
-    // console.log('body:', body);
-    // console.log('typeof body: ', typeof body);
-
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
     const data = JSON.parse(body);
-    console.log(data);
-    if (data.length !== 0) {
-      console.log('typeof data: ', typeof data);
-      console.log('id: ', data[0].id);
-      console.log('name: ', data[0].name);
-      console.log('description: ', data[0].description);
-    } else console.log('no data');
-  }
-});
+    callback(error, data[0].description);
+  });
+};
+
+
+module.exports = { fetchBreedDescription };
---
+++ index.js
@@ -0,0 +1,13 @@
+const request = require('request');
+const { fetchBreedDescription } = require('./breedFetcher');
+
+const breed = process.argv.slice(2);
+const breedName = breed[0];
+
+fetchBreedDescription(breedName, (error, desc) => {
+  if (error) {
+    console.log('Error fetch details:', error);
+  } else {
+    console.log(desc);
+  }
+});
