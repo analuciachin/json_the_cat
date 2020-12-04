6292d9eb2b24bf64c16bbc2f27247d7785bb98a3
analuciachin <ananime@gmail.com>
2020-12-04 00:55:44 +0000

    added a test for the invalid breed

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,7 +3,18 @@ const request = require('request');
 const fetchBreedDescription = function(breedName, callback) {
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
     const data = JSON.parse(body);
-    callback(error, data[0].description);
+    //console.log(data.length)
+    if (error) {
+      console.log('Error fetch details:', error);
+      callback(error, null);
+    }
+
+    if (data[0]) {
+      callback(null, data[0].description);
+    } else {
+      callback('Cannot find breed.')
+    }
+
   });
 };
 
